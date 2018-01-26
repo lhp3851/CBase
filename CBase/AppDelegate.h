84ef67a1f0f13eb9679764c1b3e5345933e50543
liuhepeng <liuhepeng@besttmkj.com>
2018-01-26 10:44:00 +0800

    Initial Commit

---
+++ CBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CBase
+//
+//  Created by Jerry on 26/01/2018.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
